6e1465bd89c627f820e17417126d82717239effd
Magnus Farstad <magnus.r.farstad@outlook.com>
2022-03-04 11:36:49 +0100

    Created server for oving 5

---
+++ oving5-server/src/main/java/student/ntnu/no/oving5server/Oving5ServerApplication.java
@@ -0,0 +1,13 @@
+package student.ntnu.no.oving5server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oving5ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oving5ServerApplication.class, args);
+	}
+
+}
---
+++ oving5-server/src/test/java/student/ntnu/no/oving5server/Oving5ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package student.ntnu.no.oving5server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oving5ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
